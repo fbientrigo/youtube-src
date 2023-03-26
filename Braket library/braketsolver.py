92e649489ee08ae82ea22bba297e268db7d5f45e
floresenhalloween <fbientrigo@gmail.com>
2023-03-26 01:00:01 -0300

    braket consrtruido como un objeto presenta mayor rendimiento y mejor estruct

--- Braket library/braketsolver.py
+++ Braket library/braketsolver.py
@@ -5,6 +5,96 @@ __MAX__ = symbols('__MAX__')
 BracketSolverLib2 = {}
 BracketSolverLib2[__MAX__] = 4
 
+
+#===========================================================
+# objeto braket
+
+from sympy import *
+
+class braket(Symbol):
+    def __new__(cls, argument, **kwargs):
+        # cls es una referencia a la clase siendo creada
+        # re escrbimos para nuevos atributos
+        obj = super().__new__(cls, str(argument), **kwargs)
+        # el inside es un symbol, se asume generalidad, puede hasta ser un symbol
+        obj.argument = argument
+        # print(type(obj.argument)) # quick debugging
+        return obj
+
+    def __str__(self):
+        return f"braket({self.argument})"
+
+    
+    def _latex(self, printer):
+        """
+        Permite incluir una representacion de printing de latex
+        """
+        return "\\langle " + printer._print(self.argument) + "\\rangle "
+
+    def args(self):
+        # experimental, al correr braket(..).args() se devuelve a si mismo
+        return (self,)
+    
+    #def __mul__(self, other):
+    #    pass
+
+    def solve(self, symbol_to_solve : Symbol, debug_print: bool = False):
+        """
+        Takes input of a symbol included inside the braket, and returns the solution
+        """
+        solutions = solve(self.argument, symbol_to_solve)
+        if debug_print:
+            print(f'number of solutions: {len(solutions)}')
+
+        if len(solutions) == 0:
+            print('no solutions found')
+        else:
+            return solutions[0]
+
+    def coefficients(self, symbol : Symbol):
+        """
+        Returns the coefficients of the symbol inside the braket.
+        """
+        return self.argument.coeff(symbol)
+
+    def modified(self, symbol : Symbol):
+        """
+        Returns a new braket object with the modified argument as described.
+        """
+        coeff_n = self.coefficients(symbol)
+        if coeff_n == 0:
+            return self
+        else:
+            new_arg = self.argument / coeff_n
+            return coeff_n * braket(new_arg)
+
+
+
+def extract_equations(expresion: Mul):
+    """
+    Extracts all equations from the argument of the multibraket.
+    Returns a list of equations.
+    """
+    equations = []
+    symbols_in_braket = []
+    for term in expresion.args:
+        if isinstance(term, braket):
+            # get the inside of a braket
+            eq = term.argument
+            equations.append(eq) # append as equation
+
+            free_symbols = eq.free_symbols #see symbols inside a braket
+            for symbol in free_symbols: 
+                if symbol not in symbols_in_braket: # add the symbol if not already added
+                    symbols_in_braket.append(symbol)
+    
+    return {'symbols': symbols_in_braket, 'equations': equations}
+
+
+
+
+
+
 #=========================================================
 def ExtractHypergeometricParameters(Function_):
  
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # youtube-src
-Go to `manim_project` for the source files of a simple manim script that works some equations of the method of brakets.
+Go to `manim_project` for the source files of a simple manim script that works some equations of the method of brakets. This github is in spanish for the sharing of the method in a youtube channel.
+
+## Carpetas
+- `Braket library`: sympy build of the method
+- 
